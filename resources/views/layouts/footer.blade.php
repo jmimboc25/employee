3b2c9c56a5f30c59c28567ee53aa658d9650a092
galahan1987 <58349040+galahan1987@users.noreply.github.com>
2021-09-24 12:09:57 +0800

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright UCTC; 2021 <a href="#">Company</a>.</strong> All rights reserved.
+  </footer>
